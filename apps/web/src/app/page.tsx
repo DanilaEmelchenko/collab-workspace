4f0382cdc6e03d5192aa80f7bcc0fbf04aec1c46
Danila Emelchenko <emelchenko1997@yandex.ru>
2026-06-22 13:36:33 +0300

    feat: setup husky+lint-staged

--- apps/web/src/app/page.tsx
+++ apps/web/src/app/page.tsx
@@ -1,8 +1,10 @@
+import { Button } from '@/shared/ui/button';
 import Image from 'next/image';
 
 export default function Home() {
   return (
     <div className='flex flex-col flex-1 items-center justify-center bg-zinc-50 font-sans dark:bg-black'>
+      <Button>Новая кнопка</Button>
       <main className='flex flex-1 w-full max-w-3xl flex-col items-center justify-between py-32 px-16 bg-white dark:bg-black sm:items-start'>
         <Image className='dark:invert' src='/next.svg' alt='Next.js logo' width={100} height={20} priority />
         <div className='flex flex-col items-center gap-6 text-center sm:items-start sm:text-left'>
