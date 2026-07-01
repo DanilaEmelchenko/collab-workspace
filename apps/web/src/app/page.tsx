84302c8c92a63557109566f622a171cea061091d
Lena_Menson <8000438@mail.ru>
2026-07-01 10:53:04 +0300

    fix: настройки pre-commit-config

--- apps/web/src/app/page.tsx
+++ apps/web/src/app/page.tsx
@@ -5,7 +5,7 @@ export default function Home() {
   return (
     <div className='flex flex-col flex-1 items-center justify-center bg-zinc-50 font-sans dark:bg-black'>
       <Button>Клик</Button>
-      <Button>Клик 1</Button>
+      <Button>Клик 2</Button>
       <main className='flex flex-1 w-full max-w-3xl flex-col items-center justify-between py-32 px-16 bg-white dark:bg-black sm:items-start'>
         <Image className='dark:invert' src='/next.svg' alt='Next.js logo' width={100} height={20} priority />
         <div className='flex flex-col items-center gap-6 text-center sm:items-start sm:text-left'>
